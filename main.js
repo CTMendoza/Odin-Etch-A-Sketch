61fc4323b569d78622947f06ba0b87e77949bffb
Christopher Mendoza <tiptop1112@gmail.com>
2024-07-13 15:35:23 -0700

    Added functionality to the button When clicking the button a prompt ask the user to enter a number to create a new grid. After the user enters the number the old grid is removed and a new grid is created with the ability to still 'draw' it.

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 // create a grid of 16 divs 
 const container = document.querySelector('#container');
 
-for(i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.className= 'square';
-    container.appendChild(square);
+function createSquare (size) {
+    for(i = 0; i < size * size; i++) {
+        let square = document.createElement('div');
+        square.style.flexBasis = `calc(100%/${size})`
+        square.className= 'square';
+        // create listener for hover event on each "square" div
+        square.addEventListener('mouseover', () => {
+            // change "square" backgroundColor when hovered over
+            square.style.backgroundColor = 'black';
+            toggleDraw(square);
+        })
+        container.appendChild(square);
+    }
+    
 }
 
- function toggleDraw (square) {
-    if(!square.style.backgroundColor) {
-        square.style.backgroundColor = 'black';
-    }
-    //  else square.style.backgroundColor = '';
- }
-// create listener for hover event on each "square" div
-const squares = container.querySelectorAll('div.square');
-squares.forEach((square) => { square.addEventListener('mouseover', () => {
-    // change "square" backgroundColor when hovered over
-    // square.style.backgroundColor = 'black';
-    toggleDraw(square);
-})});
+createSquare(16);
+
+
+//  function toggleDraw (square) {
+//     if(!square.style.backgroundColor) {
+//         square.style.backgroundColor = 'black';
+//     }
+//     //  else square.style.backgroundColor = '';
+//  }
+// select button
+
+const button =document.querySelector('#replace-grid');
+// listen for click event on button and then call prompt() so that user can fill in grid size
+button.addEventListener('click', () => {
+let size =prompt('Choose a grid size');
+if(size > 100) {
+    alert('Grid size cannot be greater than 100, choose again please :)')
+}
+//container.innerHTML=''; removes the original grid
+container.innerHTML = '';
+//calling createSquare again while passing user's prompt info creates a new grid
+createSquare(Number(size));
+})
